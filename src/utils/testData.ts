8435e8f188f69449f86093ada1046d62c8848a4b
unknown <victor.mercado@nvisionx.ai>
2025-10-17 17:05:24 -0400

    Halo QA Test for Victor Mercado

---
+++ src/utils/testData.ts
@@ -0,0 +1,10 @@
+export const userData = {
+  firstName: "Victor",
+  lastName: "Mercado",
+  email: "vic.mercado@example.com",
+  mobile: "5554649870",
+  subject: "Literature",
+  address: "5th Street",
+  state: "NCR",
+  city: "Delhi"
+};
